d17cd0a6c154e2fef950a94f994d93979d1b02c6
Britzell <alfred.brisse@gmail.com>
2020-09-03 17:31:57 +0200

    Add return mixed method get

--- src/Configuration.php
+++ src/Configuration.php
@@ -16,17 +16,22 @@ class Configuration
 		return explode('.', $var);
 	}
 	
-	public static function get(string $path)
+	public static function get(string $path, $return = \Exception::class)
 	{
 		$explode = self::explodeDot($path);
 		$content = Yaml::parse(file_get_contents(PROJECT_DIR . 'config/' . $explode[0] . '.yaml'));
 		
 		if (count($explode) > 0) {
 			foreach ($explode as $var) {
-				if (isset($content[$var]))
+				if (isset($content[$var])) {
 					$content = $content[$var];
-				else
-					throw new \Exception('"' . $path . '" not found');
+					
+				} else {
+					if ($return === \Exception::class)
+						throw new \Exception('"' . $path . '" not found');
+					else
+						return $return;
+				}
 			}
 		}
 		
